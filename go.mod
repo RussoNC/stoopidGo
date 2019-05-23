3f04155b1da477e5d03ee04160c6e66b8965cac8
Rodrigo <rodrigo.russello.prestataire@gouv.nc>
2019-05-24 09:57:03 +1100

    feat(project): all files, working version

---
+++ go.mod
@@ -0,0 +1,11 @@
+module github.com/russo/lolScript
+
+go 1.12
+
+require (
+	github.com/karrick/godirwalk v1.10.3 // indirect
+	github.com/pkg/errors v0.8.1 // indirect
+	github.com/rogpeppe/godef v1.1.1 // indirect
+	github.com/uudashr/gopkgs v2.0.1+incompatible // indirect
+	rsc.io/quote v1.5.2
+)
---
+++ main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+    "fmt"
+	"flag"
+	"strings"
+	"time"
+)
+
+func main() {
+
+	//Keeping execution time
+	start := time.Now()
+
+	//Set the flag
+	phraseFlag := flag.String("phrase", "foo", "a string")
+	flag.Parse()
+
+	//Set the string builder
+	var chainString strings.Builder
+
+	slicedFlagContent :=  strings.Split(*phraseFlag,"")
+
+	wasUpper := false
+	wasLower := false
+
+	for i, _ := range slicedFlagContent {
+
+		slicedCharacter := slicedFlagContent[i]
+
+		//Detect special characters
+		if getRune(slicedCharacter) < 'A' || getRune(slicedCharacter) > 'z'  {
+			chainString.WriteString(slicedCharacter)
+		} else {
+			if !wasLower {
+				modifiedChar := strings.ToLower(slicedCharacter)
+				chainString.WriteString(modifiedChar)
+				wasLower = true
+				wasUpper = false
+			} else if !wasUpper {
+				modifiedChar := strings.ToUpper(slicedCharacter)
+				chainString.WriteString(modifiedChar)
+				wasUpper = true
+				wasLower = false
+			}
+		}
+	}
+
+	fmt.Println(chainString.String())
+	//Get the execution time in ms
+	t := time.Now()
+	elapsed := t.Sub(start)
+	fmt.Println(elapsed.Seconds()*1e6)
+}
+
+func getRune(stringToTransform string) rune {
+	r := []rune(stringToTransform)
+	return r[0]
+}
